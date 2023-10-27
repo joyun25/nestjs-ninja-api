fbfd3bb1b2ab57e0f0d579690e2d82f936093bcc
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-27 09:50:39 +0800

    add service's code and test by Thunder Client

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -9,45 +9,41 @@ import {
   Body,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
+  constructor(private readonly ninjasService: NinjasService) {}
   // GET /ninjas --> []
-  // GET /ninjas?type=fast --> []
+  // GET /ninjas?weapon=stars --> []
   @Get()
-  getNinjas(@Query('type') type: string) {
-    return [{ type }];
+  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    // const service = new NinjasService();
+    return this.ninjasService.getNinjas(weapon);
   }
 
   // GET /ninjas/:id --> { ... }
   @Get(':id')
   getOneNinja(@Param('id') id: string) {
-    return {
-      id,
-    };
+    return this.ninjasService.getNinja(+id);
   }
 
   // POST /ninjas
   @Post()
   createNinja(@Body() createNinjaDto: CreateNinjaDto) {
-    return {
-      name: createNinjaDto.name,
-    };
+    return this.ninjasService.createNinja(createNinjaDto);
   }
 
   // PUT /ninjas/:id --> { ... }
   @Put(':id')
-  updateNinja(@Param('id') id: string) {
-    return {
-      id,
-    };
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return this.ninjasService.updateNinja(+id, updateNinjaDto);
   }
 
   // DELETE /ninjas/:id
   @Delete(':id')
   removeNinja(@Param('id') id: string) {
-    return {
-      id,
-    };
+    return this.ninjasService.removeNinja(+id);
   }
 }
